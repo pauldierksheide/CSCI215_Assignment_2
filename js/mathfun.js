9f84d15405000a95a7b8013b81fe99b491dfcbfc
pauldierksheide <pauldierksheide@gmail.com>
2018-10-21 12:04:21 -0400

    commit

--- js/mathfun.js
+++ js/mathfun.js
@@ -18,11 +18,44 @@ function enterNum() {
 
     console.log(numArray);
 
+    //FINDING SUM
+    var sum = 0;
+    var intNum;
+    for (var i=0; i<numArray.length; i++) {
+        intNum = parseInt(numArray[i],10);
+        sum += intNum;
+    }
+    console.log(sum);
 
-    //Math functions
-    //maximum, minimum, mean, and median
+    //FINDING MAX
+    var maxVal = Math.max.apply(null, numArray);
+    console.log(maxVal);
+
+    //FINDING MIN
+    var minVal = Math.min.apply(null, numArray);
+    console.log(minVal);
+
+    //FINDING MEAN
+    var mean = sum/numArray.length;
+    console.log(mean);
+
+    //FINDING MEDIAN
+    var median = 0
+    numArray.sort();
+    if (numArray.length % 2 === 0) {
+        console.log("Num1: " + parseInt(numArray[numArray.length/2 - 1], 10));
+        console.log("Num2: " + parseInt(numArray[numArray.length / 2],10));
+        var num1 = parseInt(numArray[numArray.length/2 - 1], 10);
+        var num2 = parseInt(numArray[numArray.length / 2],10)
+        median = (num1 + num2)/2;
+    } else {
+        median = parseInt(numArray[(numArray.length - 1) / 2],10);
+    }
+
+    console.log(median);
+
     //even numbers, odd numbers, prime numbers, and power-of-2 numbers
 
-    //output results
+    //output results onto the screen
 
 }
